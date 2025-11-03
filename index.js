44ea37c16aa1d15ea4342a3bd2b2df265883b092
kirillbbb <kirillbalandin2526@gmail.com>
2025-11-03 22:15:19 +0300

    tasks 1-2

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log('Happy developing ✨')
+//Задание 1
+function sum(a) {
+    let curSum = a;
+
+    function insideFunc(b) {
+        if (b !== undefined) {
+            curSum += b;
+            return insideFunc;
+        }
+        return curSum;
+    }
+
+    insideFunc.result = function() {
+        return curSum;
+    };
+
+    return insideFunc;
+}
+
+console.log(sum(2)(3)(5).result());
+
+//Задание 2
+const str = 'one.two.three.four.five';
+
+function strToObj(str) {
+    const elements = str.split('.');
+    const resultObj = {};
+
+    let curLevel = resultObj;
+
+    for (let elem of elements) {
+        curLevel[elem] = {};
+        curLevel = curLevel[elem];
+    }
+
+    return resultObj;
+}
+
+console.log(strToObj(str)); //не совсем правильно выводит
+
+console.log(JSON.stringify(strToObj(str),null,1));
